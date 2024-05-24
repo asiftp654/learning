c88dd8b7529255119869b62626843ea323f81b33
Asif <asif.s@qburst.com>
2024-05-24 22:36:04 +0530

    second commit

--- learning/settings.py
+++ learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qqlg)86xm)kvonm)4gl8le6d2@5cpo&^*^ii6sd1)nipsg286@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['23.20.190.98', 'localhost', '0.0.0.0']
 
 
 # Application definition
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
